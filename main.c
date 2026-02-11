a860de362372d64114a65bc732261aa843578a63
Brando Calderara <151777266+BrandosQuest@users.noreply.github.com>
2026-02-11 23:57:36 +0100

    now working on ball and rect game

--- main.c
+++ main.c
@@ -3,13 +3,30 @@
 
 int main(void)
 {
-    InitWindow(800, 450, "raylib [core] example - basic window");
+    int width=800, height=450;
+    InitWindow(width, height, "raylib [core] example - basic window");
+    SetTargetFPS(60);
+
+    int shuttleXpos=width/2;
+    int shuttleWidth=80;
+    int shuttleHeight=20;
+    bool gameON=false;
 
     while (!WindowShouldClose())
     {
+        //update stuff
+        if (!gameON)
+        {
+            gameON=IsMouseButtonReleased(MOUSE_LEFT_BUTTON);
+        }
+        shuttleXpos=GetMouseX();
+
+        //draw stuff
         BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        ClearBackground(DARKGREEN);
+        //DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        DrawRectangle(shuttleXpos-(shuttleWidth/2), height-shuttleHeight, shuttleWidth, shuttleHeight, BLACK);
+        DrawFPS(0,0);
         EndDrawing();
     }
 
