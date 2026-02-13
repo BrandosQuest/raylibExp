666e726c5c8831ece1a1ab5b57336d7a200426ec
Brando Calderara <151777266+BrandosQuest@users.noreply.github.com>
2026-02-13 15:17:42 +0100

    just to keep things committed

--- main.c
+++ main.c
@@ -11,6 +11,10 @@ int main(void)
     int shuttleWidth=80;
     int shuttleHeight=20;
     bool gameON=false;
+    Vector2 ballVelocity={0,0};
+    Vector2 ballPosition={0,0};
+    float ballRadius=10;
+    float attenuation=1;
 
     while (!WindowShouldClose())
     {
@@ -18,13 +22,21 @@ int main(void)
         if (!gameON)
         {
             gameON=IsMouseButtonReleased(MOUSE_LEFT_BUTTON);
+            ballVelocity.x=1;
+        }
+        else
+        {
+            ballVelocity.x=1;
         }
         shuttleXpos=GetMouseX();
+        ballPosition.x+=ballVelocity.x*attenuation;
+        ballPosition.y+=ballVelocity.y*attenuation;
 
         //draw stuff
         BeginDrawing();
         ClearBackground(DARKGREEN);
         //DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        DrawCircle(ballPosition.x ,ballPosition.y ,ballRadius, LIGHTGRAY);
         DrawRectangle(shuttleXpos-(shuttleWidth/2), height-shuttleHeight, shuttleWidth, shuttleHeight, BLACK);
         DrawFPS(0,0);
         EndDrawing();
